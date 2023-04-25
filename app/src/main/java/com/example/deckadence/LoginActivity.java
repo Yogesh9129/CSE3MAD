c36899f5129f8b325baeff656ed23ab2a243f589
Mach565 <fordgtrace246@protonmail.com>
2023-04-25 15:33:12 +1000

    Authentication now works.

--- app/src/main/java/com/example/deckadence/LoginActivity.java
+++ app/src/main/java/com/example/deckadence/LoginActivity.java
@@ -1,17 +1,30 @@
 package com.example.deckadence;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
 public class LoginActivity extends AppCompatActivity {
+    private GoogleSignInOptions googleSignInOptions;
+    private GoogleSignInClient googleSignInClient;
     private TextView title;
-    private EditText username;
-    private EditText password;
+    //private EditText username;
+    //private EditText password;
+    //private Button login_button;
     private Button googleLogin;
 
     @Override
@@ -20,19 +33,39 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         // set up layout
         layoutSetup();
-        //mAuth = FirebaseAuth.getInstance();
     }
 
     private void layoutSetup() {
-        title = (TextView) findViewById(R.id.deckadence_title);
-        username = (EditText) findViewById(R.id.editTextUsername);
-        password = (EditText) findViewById(R.id.editTextPassword);
-        googleLogin = (Button) findViewById(R.id.login_with_google);
+        title = findViewById(R.id.deckadence_title);
+        //username = findViewById(R.id.username);
+        //password = findViewById(R.id.password);
+        //login_button = findViewById(R.id.login_button);
+        googleLogin =  findViewById(R.id.login_with_google);
+        googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
         googleLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                {
+                    Intent intent = googleSignInClient.getSignInIntent();
+                    startActivityForResult(intent, 100);
+                }
             }
         });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 100) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try{
+                task.getResult(ApiException.class);
+                finish();
+            } catch (ApiException e) {
+                // error should probably be more specific
+                Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/deckadence/MainActivity.java
+++ app/src/main/java/com/example/deckadence/MainActivity.java
@@ -3,13 +3,19 @@ package com.example.deckadence;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-//import com.google.firebase.auth.FirebaseUser;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,15 +23,19 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private boolean loggedIn = false;
     private TextView test;
+    private Button logoutButton;
+    private GoogleSignInOptions googleSignInOptions;
+    private GoogleSignInClient googleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("deckadence", "Reached here");
-        // check if logged in here
-        if(!loggedIn) { // also check for internet
-            Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
-            MainActivity.this.startActivity(loginIntent);
+        googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+        // googleSignInClient is not null if logged in
+        loggedIn = googleSignInClient != null;
+        if(!loggedIn) { // also check for internet mayve
+            MainActivity.this.startActivity(new Intent(getApplicationContext(), LoginActivity.class));
         }
         setContentView(R.layout.activity_main);
         layoutSetup();
@@ -34,5 +44,19 @@ public class MainActivity extends AppCompatActivity {
     private void layoutSetup() {
         //this is just for testing, feel free to remove it if you need to
         test = findViewById(R.id.textView);
+        logoutButton = findViewById(R.id.logout_button);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("deckadence","logging out");
+                googleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Log.d("deckadence","logged out");
+                        MainActivity.this.startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                    }
+                });
+            }
+        });
     }
 }
