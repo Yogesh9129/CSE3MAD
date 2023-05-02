07ae1a9ea49eeb691d2b85f597d8b599d522a5e3
Mach565 <fordgtrace246@protonmail.com>
2023-05-02 21:57:55 +1000

    The bar doesn't work. But the app doesn't crash on load either. Good enough for now.

--- app/src/main/java/com/example/deckadence/MainActivity.java
+++ app/src/main/java/com/example/deckadence/MainActivity.java
@@ -4,6 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,14 +16,13 @@ import android.widget.TextView;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private boolean loggedIn = false;
-    private TextView test;
-    private Button logoutButton;
+    private boolean loggedIn;
     private GoogleSignInOptions googleSignInOptions;
     private GoogleSignInClient googleSignInClient;
 
@@ -28,7 +32,11 @@ public class MainActivity extends AppCompatActivity {
         googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
         googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
         // googleSignInClient is not null if logged in
-        loggedIn = googleSignInClient != null;
+        try {
+            loggedIn = googleSignInClient != null;
+        } catch (NullPointerException e) {
+            loggedIn = false;
+        }
         if(!loggedIn) { // also check for internet maybe
             MainActivity.this.startActivity(new Intent(getApplicationContext(), LoginActivity.class));
         }
@@ -37,21 +45,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void layoutSetup() {
-        //this is just for testing, feel free to remove it if you need to
-        //test = findViewById(R.id.textView);
-        //logoutButton = findViewById(R.id.logout_button);
-        /*logoutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d("deckadence","logging out");
-                googleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        Log.d("deckadence","logged out");
-                        MainActivity.this.startActivity(new Intent(getApplicationContext(), LoginActivity.class));
-                    }
-                });
-            }
-        }); */
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_decks, R.id.navigation_settings)
+                .build();
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_activity_main);
+        NavController navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(navView, navController);
     }
 }
